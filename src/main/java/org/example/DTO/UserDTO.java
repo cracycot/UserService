2071a5263a98016c512f248bd66c164d0d693254
cracycot <cracycot@mail.ru>
2024-12-11 16:07:17 +0300

    feat: made DTO

---
+++ src/main/java/org/example/DTO/UserDTO.java
@@ -0,0 +1,51 @@
+package org.example.DTO;
+
+import java.util.Date;
+
+public class UserDTO {
+    private String name;
+    private String lastname;
+    private String email;
+    private String status;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
